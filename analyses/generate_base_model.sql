455d40a339957ba35a079f2729d065946fb0964d
David Reynolds <davidgazeyreynolds@gmail.com>
2025-11-20 18:57:05 +0000

    created staging model for feedback_themes table in customer_support

--- analyses/generate_base_model.sql
+++ analyses/generate_base_model.sql
@@ -1,5 +1,5 @@
 {{ codegen.generate_base_model(
-    source_name='crm_tenants',
-    table_name='tenants',
+    source_name='customer_support',
+    table_name='feedback_themes',
     materialized='table'
 ) }}
