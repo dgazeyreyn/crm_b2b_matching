51ec64b25f147903f92f23a1744973dca7084923
David Reynolds <davidgazeyreynolds@gmail.com>
2025-11-21 16:25:21 +0000

    created staging model for companies table in zi_companies

---
+++ models/staging/zi_companies/stg_zi_companies__companies.sql
@@ -0,0 +1,17 @@
+with source as (
+
+    select * from {{ source('zi_companies', 'companies') }}
+
+),
+
+renamed as (
+
+    select
+        zi_company_id,
+        zi_company_name
+
+    from source
+
+)
+
+select * from renamed
