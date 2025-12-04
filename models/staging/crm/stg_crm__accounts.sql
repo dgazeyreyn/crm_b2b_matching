ee94571f9317be7c996878d50851104fa2b50dc2
David Reynolds <davidgazeyreynolds@gmail.com>
2025-12-04 15:40:40 +0000

    renamed models

--- models/staging/crm/stg_crm__accounts.sql
+++ models/staging/crm/stg_crm__accounts.sql
@@ -1,5 +1,5 @@
 with
-    source as (select * from {{ source("crm", "customer_crm_accounts") }}),
+    source as (select * from {{ source("crm", "accounts") }}),
 
     renamed as (select crm_account_id, crm_account_name, tenant_id from source)
 
