93c31a1ff7dd0de579e0522238615491d740c65e
David Reynolds <davidgazeyreynolds@gmail.com>
2025-12-10 16:42:29 +0000

    fixed query to include aliases for all cities

--- models/marts/dim_city_region_ref.sql
+++ models/marts/dim_city_region_ref.sql
@@ -1,9 +1,9 @@
 -- models/dimensions/dim_city_region_ref.sql
 {{ config(materialized='table') }}
 
-select 'Austin' as city, 'Texas' as region union all
-select 'Boston', 'Massachusetts' union all
-select 'Chicago', 'Illinois' union all
-select 'New York', 'New York' union all
-select 'San Francisco', 'California' union all
-select 'Seattle', 'Washington'
+select 'Austin'        as zi_company_city, 'Texas'         as region union all
+select 'Boston'        as zi_company_city, 'Massachusetts' as region union all
+select 'Chicago'       as zi_company_city, 'Illinois'      as region union all
+select 'New York'      as zi_company_city, 'New York'      as region union all
+select 'San Francisco' as zi_company_city, 'California'    as region union all
+select 'Seattle'       as zi_company_city, 'Washington'    as region
